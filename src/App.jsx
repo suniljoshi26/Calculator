6de24e17547db7243bb4d06562bf595a43d8efb6
Sunil Joshi <joshisunil2601@gmail.com>
2022-11-08 11:34:55 +0530

    css file

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,32 @@
 import React from "react";
+import { Button } from "./Button";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <input />
+      <div>
+        <Button>1</Button>
+        <Button>1</Button>
+        <Button>1</Button>
+        <Button>1</Button>
+        <Button>1</Button>
+        <Button>1</Button>
+        <Button>1</Button>
+        <Button>1</Button>
+        <Button>1</Button>
+        <Button>1</Button>
+        <Button>1</Button>
+        <Button>1</Button>
+        <Button>1</Button>
+        <Button>1</Button> <Button>1</Button>
+        <Button>1</Button>
+        <Button>1</Button>
+        <Button>1</Button>
+        <Button>1</Button>
+        <Button>1</Button>
+      </div>
+    </div>
+  );
 };
 export default App;
---
+++ src/Button.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Button = ({ children }) => {
+  return (
+    <div>
+      <button>{children}</button>
+    </div>
+  );
+};
